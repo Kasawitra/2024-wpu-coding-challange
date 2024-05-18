c0c28fbd9bf5ec278686c69b2b07c66112b7c1be
KirigayaKazuto91 <mike_shadowz91@protonmail.com>
2024-05-18 12:31:10 +0700

    day-2: power of two

--- 1-grow.js
+++ 1-grow.js
@@ -13,4 +13,8 @@
 
 const grow = (x) => x.reduce((acc, cur) => acc * cur, 1);
 
-console.log(grow([1, 2, 3, 4]));
+{
+  {
+    console.log(grow([1, 2, 3, 4]));
+  }
+}
---
+++ 2-poweroftwo.js
@@ -0,0 +1,13 @@
+function powersOfTwo(n) {
+  var result = [];
+  for (var i = 0; i <= n; i++) {
+    result.push(Math.pow(2, i));
+  }
+  return result;
+}
+
+{
+  {
+    console.log(powersOfTwo(4));
+  }
+}

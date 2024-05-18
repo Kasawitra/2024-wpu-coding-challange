a73e0d55066c068bd8147e0e6c46584db6b4c400
KirigayaKazuto91 <mike_shadowz91@protonmail.com>
2024-05-18 12:46:55 +0700

    day-3: sentences smash

---
+++ 3-sentencesmash.js
@@ -0,0 +1,12 @@
+// function smash(words) {
+//   var smashedWords = words.join(" ");
+//   return smashedWords;
+// }
+
+const smash = (words) => words.join(" ");
+
+{
+  {
+    console.log(smash(["Hello", "world", "this", "is", "smash"]));
+  }
+}
